b5bd4d70faeeb6fcf2e9e5e3e665f7a76c3e0f4e
tranphuchau-agu <tphau_21th@student.agu.edu.vn>
2025-08-27 23:21:16 +0700

    Add files via upload

---
+++ vd.chuong1.py
@@ -0,0 +1,21 @@
+print('một người không bao giờ mắc sai lầm không bao giờ thử điều gì mới')
+
+if 5 > 2:
+    print('five is greater than two!')
+
+    total =1 + \
+    2+ \
+    3
+print(total); print('hello python')
+
+if True:
+    print('true')
+else:
+    print('false')  
+
+    #this is a comment
+    print("Hello, World!") 
+
+    def add(a, b):
+     result = a + b
+     return result
