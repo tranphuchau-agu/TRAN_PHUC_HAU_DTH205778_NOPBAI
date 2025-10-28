6cc5e8c6880c80848755526137bd85f988ea2b3d
Phúc_Hậu <tphau_21th@student.agu.edu.vn>
2025-10-28 14:06:28 +0700

    Delete Cau_01.py

--- Cau_01.py
+++
@@ -1,7 +0,0 @@
-# Kiểm tra năm nhuần
-print("Chương trình kiểm tra năm nhuần\n")
-year = int(input("Nhập vào một năm: "))
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(f"Năm {year} là năm nhuần")
-else:
-    print(f"Năm {year} không phải là năm nhuần")
